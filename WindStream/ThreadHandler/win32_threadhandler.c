9c5a8bbd36972994a2d3e0c32ad9eb78a70d06e3
David Weil <gamemx25@gmail.com>
2019-10-19 23:29:52 -0700

    Designing ThreadHandler

---
+++ WindStream/ThreadHandler/win32_threadhandler.c
@@ -0,0 +1,9 @@
+#include "win32_threadhandler.h"
+
+typedef struct win32_thread {
+	void* value;
+} WIN32THREAD;
+
+void CreateThread() {
+
+}
---
+++ WindStream/ThreadHandler/win32_threadhandler.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#if defined(_WIN32)
+#include <windows.h>
+#endif
