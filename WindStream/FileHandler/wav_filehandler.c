ff5d4dc78f2913a8df464591d6d9dada2e607d00
David Weil <gamemx25@gmail.com>
2019-10-27 18:15:16 -0700

    Add SubchunkSize1_16 structure

--- WindStream/FileHandler/wav_filehandler.c
+++ WindStream/FileHandler/wav_filehandler.c
@@ -4,6 +4,7 @@
 typedef struct wav_file {
 	FILE* fp;
 	byte fileState;
+	WAVHEADER* header;
 } WAVFILE;
 
 WAVFILE* CreateWavFile() {
@@ -15,6 +16,16 @@ WAVFILE* CreateWavFile() {
 
 	newFile->fp = NULL;
 	newFile->fileState = UNSET;
+	newFile->header = malloc(sizeof(WAVHEADER));
+
+	if (newFile->header == NULL) {
+		// Error
+		free(newFile);
+		return NULL;
+	}
+
+	newFile->header->riff = malloc(sizeof(RIFFCHUNK));
+	newFile->header->fmt = malloc(sizeof(FMTCHUNK));
 
 	return newFile;
 }
@@ -36,5 +47,49 @@ void OpenWavFile(WAVFILE* ptr, char* filename) {
 }
 
 void ReadWavHeader(WAVFILE* ptr) {
+	if (ptr->fileState != OPEN) return;
 
+	size_t rc;
+	RIFFCHUNK* riff = ptr->header->riff;
+	FMTCHUNK* fmt = ptr->header->fmt;
+
+	rc = fread(riff, sizeof(RIFFCHUNK), 1, ptr->fp);
+	if (rc != 1 || strcmp("RIFF", riff->ChunkId)) return;
+
+	rc = fread(fmt->Subchunk1ID, sizeof(byte), 4, ptr->fp);
+	if (rc != 4 || strcmp("fmt ", fmt->Subchunk1ID)) return;
+
+	fread(fmt->Subchunk1Size, sizeof(int), 1, ptr->fp);
+
+	switch (fmt->Subchunk1Size) {
+		case 16:
+			fmt->details = malloc(sizeof(FMTDETAILCHUNK16));
+			ReadDetailsSize16(ptr->fp, fmt->details);
+			break;
+		case 18:
+			break;
+		case 40:
+			break;
+		default:
+			// Error out
+			break;
+	}
+
+	return;
 }
+
+// Private Functions //
+
+void ReadDetailsSize16(FILE* fp, FMTDETAILCHUNK16* details) {
+	fread(details->AudioFormat, sizeof(short), 1, fp);
+	fread(details->NumChannels, sizeof(short), 1, fp);
+	fread(details->SampleRate, sizeof(int), 1, fp);
+	fread(details->ByteRate, sizeof(int), 1, fp);
+	fread(details->BlockAlign, sizeof(short), 1, fp);
+	fread(details->BitsPerSample, sizeof(short), 1, fp);
+	return;
+}
+
+//size_t fread(void* ptr, size_t size_of_elements, size_t number_of_elements, FILE* a_file);
+
+//size_t fwrite(const void* ptr, size_t size_of_elements, size_t number_of_elements, FILE* a_file);
--- WindStream/FileHandler/wav_filestructure.h
+++ WindStream/FileHandler/wav_filestructure.h
@@ -3,8 +3,36 @@
 #ifndef __WAVFILESTRUCTURE__INCLUDED__
 #define __WAVFILESTRUCTURE__INCLUDED__
 
-typedef struct wav_structure {
-	void* value;
-} WAVSTRUCTURE;
+typedef struct riff_chunk {
+	byte ChunkId[4];
+	int ChunkSize;
+	byte Format[4];
+} RIFFCHUNK;
+
+typedef struct fmt_chunk {
+	byte Subchunk1ID[4];
+	int Subchunk1Size;
+	void* details;
+} FMTCHUNK;
+
+typedef struct detail_chunk_16 {
+	short AudioFormat;
+	short NumChannels;
+	int SampleRate;
+	int ByteRate;
+	short BlockAlign;
+	short BitsPerSample;
+} FMTDETAILCHUNK16;
+
+typedef struct wav_header {
+	RIFFCHUNK* riff;
+	FMTCHUNK* fmt;
+} WAVHEADER;
+
+typedef struct wav_data {
+	byte Subchunk2ID[4];
+	unsigned int Subchunk2Size;
+
+} WAVDATA;
 
 #endif // !__WAVFILESTRUCTURE__INCLUDED__
